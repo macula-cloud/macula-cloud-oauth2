0f1cd77f3e2a9cfcf9edb4645e8f33d547eb17a0
Jokeway <jokeway@hotmail.com>
2020-07-07 17:33:47 +0800

    [IMP] update chart liveness & readiness

--- src/main/java/org/macula/cloud/oauth2/controller/UserInfoEndpoint.java
+++ src/main/java/org/macula/cloud/oauth2/controller/UserInfoEndpoint.java
@@ -1,8 +1,5 @@
 package org.macula.cloud.oauth2.controller;
 
-import org.macula.cloud.core.principal.SubjectPrincipal;
-import org.macula.cloud.core.principal.SubjectPrincipalSessionStorage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,19 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserInfoEndpoint {
 
-	@Autowired
-	private SubjectPrincipalSessionStorage sessionStorage;
-
 	@GetMapping(value = { "/user/me", "/api/user/me" })
 	public Object user(Authentication authentication) {
-		if (authentication != null && authentication.getPrincipal() instanceof SubjectPrincipal) {
-			String guid = ((SubjectPrincipal) authentication.getPrincipal()).getUserId();
-			SubjectPrincipal principal = sessionStorage.checkoutPrincipal(guid);
-			if (principal != null) {
-				return principal;
-			}
-		}
-
 		return authentication;
 	}
 
